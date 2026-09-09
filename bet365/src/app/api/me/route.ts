79e9a6fae02d3e15bebca969731ff2314ade9582
kri114 <kriklei.abdi@gmail.com>
2026-09-09 15:23:33 +0200

    Delete bet365 directory

--- bet365/src/app/api/me/route.ts
+++
@@ -1,8 +0,0 @@
-import { getSessionUser } from "@/lib/auth";
-
-export const dynamic = "force-dynamic";
-
-export async function GET() {
-  const user = await getSessionUser();
-  return Response.json({ user });
-}
